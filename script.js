ae18e87f56cc51601b4fc388c347213e39073db9
Alex Defrese <aidandefrese@gmail.com>
2022-08-30 18:08:36 -0400

    fixed local storage persistance and background color

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ var currentHour = moment().format('HH');
 // displays time
 $("#currentDay").html(currentTime);
 
-
+// changes background depending on past/present/future compared to current time
 function backgroundColor () {
- $(".form-control").each(function () {
-   var schTime = parseInt($(this).attr("id"));
+ $(".form-inline").each(function () {
+   var schTime = ($(this).attr("id"));
    var hour =  parseInt(currentHour);
 
    if (hour > schTime) {
@@ -26,9 +26,33 @@ function backgroundColor () {
 
 backgroundColor ();
 
-$("#saveBtn").on("click",
-    function () {
-        localStorage.setItem('form-control', form-control);
+// save button saves form submission on click
+
+$(".saveBtn").on("click",
+    function (event) {
+        var value = $(this).siblings('.form-control').val();
+        var key = $(this).parent().parent().attr('id');
+        console.log(key);
+        localStorage.setItem(key, value);
     }
 );
-console.log(localStorage);
+
+// set local storage for each hour
+$('#9').children('.col-sm-10').children('.form-control').val(localStorage.getItem('9'));
+
+$('#10').children('.col-sm-10').children('.form-control').val(localStorage.getItem('10'));
+
+$('#11').children('.col-sm-10').children('.form-control').val(localStorage.getItem('11'));
+
+$('#12').children('.col-sm-10').children('.form-control').val(localStorage.getItem('12'));
+
+$('#13').children('.col-sm-10').children('.form-control').val(localStorage.getItem('13'));
+
+$('#14').children('.col-sm-10').children('.form-control').val(localStorage.getItem('14'));
+
+$('#15').children('.col-sm-10').children('.form-control').val(localStorage.getItem('15'));
+
+$('#16').children('.col-sm-10').children('.form-control').val(localStorage.getItem('16'));
+
+$('#17').children('.col-sm-10').children('.form-control').val(localStorage.getItem('17'));
+
